56b3144f5624d47387af22a82dad61864d066c35
Mackenzie-Frey <mackenziefrey1@gmail.com>
2018-10-31 09:24:22 -0600

    it1 photo lcass complete

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,8 +1,11 @@
 class Photograph
-#  attr_reader :attributes
+  attr_reader :id, :name, :artist_id, :year
 
   def initialize(attributes)
-    @attributes = attributes
+    @id = attributes[:id].to_i
+    @name = attributes[:name]
+    @artist_id = attributes[:artist_id]
+    @year = attributes[:year]
   end
 
 
